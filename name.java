33926975a1834cf1e8384a32b478cda3bd2ad0b5
ishitamehta663 <154406479+ishitamehta663@users.noreply.github.com>
2024-08-03 14:02:52 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,16 @@
+class name{
+    public static void main(String [] args)
+    {
+        String firstname="tony";
+        String lastname="stark";
+        String fullname=firstname +"@"+ lastname;
+
+        System.out.println(fullname.length());
+
+        for(int i=0;i<fullname.length();i++)
+        {
+            System.out.println(fullname.charAt(i));
+        }
+
+    }
+}
