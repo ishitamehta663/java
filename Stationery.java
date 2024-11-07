a0776a3205e7bf3923d621a58e2d210726a9a266
ishitamehta663 <154406479+ishitamehta663@users.noreply.github.com>
2024-11-07 14:31:42 +0530

    Add files via upload

---
+++ Stationery.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Stationery{
+
+		public static void main(String[] args)
+	{
+
+		Map<String, Integer>
+			table = new Hashtable<>();
+
+		table.put("Pen", 10);
+		table.put("Book", 500);
+		table.put("Clothes", 400);
+		table.put("Mobile", 5000);
+		table.put("Booklet", 2500);
+
+		table.forEach((k, v) -> {
+
+			v = v + 100;
+			table.replace(k, v);
+		});
+
+		table.forEach(
+			(k, v) -> System.out.println("Key : " + k + ", Value : " + v));
+	}
+}
