e11844c01109801fb5ed1a32895c3fd2e5776965
ishitamehta663 <154406479+ishitamehta663@users.noreply.github.com>
2025-01-23 16:44:44 +0530

    Add files via upload

---
+++ arrayM.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class arrayM
+{
+  public static void main(String [] args){
+    ArrayList<String> a = new ArrayList<String>();
+    a.add("a");
+    a.add("b");
+    a.add("c");
+    a.add("null");
+    a.add("null");
+    a.add("null");
+
+    System.out.println(a.toString());
+  } 
+}
